0a2183da30183c5c552742a54d6b76507eeb35d3
Romano_M <deep.depp@yandex.com>
2020-09-15 19:55:14 +0300

    Add navigation to new contact fragment

--- app/src/main/java/com/example/contacts/contactsscreen/ContactsFragment.kt
+++ app/src/main/java/com/example/contacts/contactsscreen/ContactsFragment.kt
@@ -5,10 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.contacts.R
+import kotlinx.android.synthetic.main.fragment_contacts.*
 
 
 class ContactsFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ContactsFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,8 +24,12 @@ class ContactsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_contacts, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() = ContactsFragment()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fabNewContact.setOnClickListener { openNewContact() }
+    }
+
+    private fun openNewContact() {
+        findNavController().navigate(R.id.action_contactsFragment_to_newContactFragment)
     }
 }
